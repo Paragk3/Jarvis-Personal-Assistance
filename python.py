6339635c4d38e8899c426d68cbe819f997cf93f4
username <user_khirade36@gmal.com>
2023-08-28 13:41:19 +0530

    chnage directory

---
+++ python.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.speed(200)
+turtle.bgcolor("black")
+for i in range(3):
+    for colors in ['red','magenta','blue','cyan','green','purple','orange','yellow','black','white']
+    turtle.color(colors)
+       turtle.width(3)
+       turtle.It(12)
+       for j in range(5):
+          turtle.fd(150)
+          turtle.It(72)
+turtle.exitonclick()
